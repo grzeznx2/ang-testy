0046ed81e8db25fb1de35244edd183fd8c58e065
grzeznx2 <grzeznx2@gmail.com>
2022-04-04 13:50:44 +0200

    create initial auth folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegisterComponent, LoginComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
