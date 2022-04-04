f8fcdb541af04d4c8532ffbe6db1d7b3b7621f97
grzeznx2 <grzeznx2@gmail.com>
2022-04-04 16:19:07 +0200

    initialize registerForm in separate method

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent  {
-
-  public registerForm = this.formBuilder.group({
-    email: ['',[ Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(8)]],
-    isAdmin: [false]
-  })
+export class RegisterComponent implements OnInit {
+public registerForm!: FormGroup
 
   constructor(private formBuilder: FormBuilder) {
 
   }
 
+ ngOnInit(): void {
+   this.registerForm = this._createForm()
+ }
+
+ private _createForm() {
+  return this.formBuilder.group({
+    email: ['',[ Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    isAdmin: [false]
+  })
+ }
 
 }
