8edb24a7f3880ad59485f1a9f89244560cd48dd9
grzeznx2 <grzeznx2@gmail.com>
2022-04-05 10:42:03 +0200

    add logic for registering user

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
+import { Role, UserPayload } from './types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private user = new BehaviorSubject<{roles: Role[], email: string} | null>(null)
+
+  constructor(private http: HttpClient) { }
+
+  get user$(){
+    return this.user.asObservable()
+  }
+
+  setUser(user: {roles: Role[], email: string} | null){
+    this.user.next(user)
+  }
+
+  register(user: UserPayload){
+   return this.http.post('http://localhost:3000/users', user).pipe(tap(()=>this.setUser(user)))
+  }
+
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +10,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 public registerForm!: FormGroup
 
-  constructor(private formBuilder: FormBuilder) {
-
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
   }
 
  ngOnInit(): void {
    this.registerForm = this._createForm()
+   this.authService.user$.subscribe(console.log)
+ }
+
+ onSubmit(){
+  const {isAdmin, ...rest} = this.registerForm.value
+  this.authService.register({...rest, roles: isAdmin? ['USER', 'AUTHOR']: ['USER']}).subscribe()
  }
 
  private _createForm() {
