d87183503b483824149a3bbba4da63343408ba3f
grzeznx2 <grzeznx2@gmail.com>
2022-04-05 19:20:51 +0200

    display recipe form only if user is AUTHOR

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { Role } from '../auth/types';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public user: {email: string, roles: Role[]} | null = null
+  private _subscriptions: Subscription[] = []
+
+  constructor(private authService: AuthService) { }
+
+  get isAuthor(){
+    return this.user?.roles.includes('AUTHOR')
+  }
 
   ngOnInit(): void {
+   this._subscriptions.push(this.authService.user$.subscribe(result=> this.user = result))
+  }
+
+  ngOnDestroy(): void {
+    this._subscriptions.forEach(subscription=>subscription.unsubscribe())
   }
 
 }
