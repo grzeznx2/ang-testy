029602ec4ebe9911538abd516b9a19301ffc385c
grzeznx2 <grzeznx2@gmail.com>
2022-04-05 16:44:16 +0200

    create route guards

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { map, tap } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router){
+
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot
+    ) {
+      return this.authService.user$.pipe(map((user)=>{
+        if(user) return !!user
+       return this.router.createUrlTree(['auth'])
+      }))
+  }
+
+}
