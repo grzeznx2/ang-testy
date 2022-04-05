93104d0c6b1a349081e00881963f53dc55186057
grzeznx2 <grzeznx2@gmail.com>
2022-04-05 14:49:02 +0200

    enable logout if user is logged in

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {  Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { Role } from '../auth/types';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public user: {email: string, roles: Role[]} | null = null
+  private _subscriptions: Subscription[] = []
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+   this._subscriptions.push(this.authService.user$.subscribe(result=> this.user = result))
   }
 
+  ngOnDestroy(): void {
+    this._subscriptions.forEach(subscription=>subscription.unsubscribe())
+  }
+
+  logout(){
+    this.authService.logout()
+  }
 }
